0d287a6770946f14f0192e61bb2528b68b430ca2
silasmas <silasjmas@gmail.com>
2022-10-06 09:40:19 +0100

    oekde

--- app/Http/Livewire/Dashbord.php
+++ app/Http/Livewire/Dashbord.php
@@ -8,30 +8,40 @@ use Livewire\Component;
 class Dashbord extends Component
 {
     public $date;
-    public $all=[];
+    public $all = [];
 
-    public function byDate(){
-        $this->all=client::selectRaw("clients.*,users.name un,users.prenom up,client_libelles.*")->orderBy('client_libelles.created_at',"ASC")
-        ->join("client_libelles","clients.id","client_libelles.client_id")
-        ->join("users","users.id","client_libelles.user_id")
-        ->where([['client_libelles.created_at',$this->date." 00:00:00"]])
-        ->get();
-        // dd($this->all);
+    protected $listeners = ['actualise' => '$refresh'];
+    public function byDate()
+    {
+        // dd($this->date);
+        $this->all = client::selectRaw("clients.*,users.name un,users.prenom up,client_libelles.*")->orderBy('client_libelles.created_at', "ASC")
+            ->join("client_libelles", "clients.id", "client_libelles.client_id")
+            ->join("users", "users.id", "client_libelles.user_id")
+            ->whereBetween('client_libelles.created_at', [$this->date . " 00:00:00", $this->date . " 23:59:59"])
+            ->get();
+            $this->emitself("actualise");
+        //dd($this->all);
+
+    }
+    public function mount(){
+
 
     }
     public function render()
     {
-    //     $today = date("Y-m-d h:m:s");
-    //     $byAgent=client::selectRaw("clients.*,client_libelles.*")->orderBy('client_libelles.created_at',"ASC")
-    //     ->join("client_libelles","clients.id","client_libelles.client_id")
-    //    ->get();
-        $this->all=client::selectRaw("clients.*,users.name un,users.prenom up,client_libelles.*")->orderBy('client_libelles.created_at',"ASC")
-        ->join("client_libelles","clients.id","client_libelles.client_id")
-        ->join("users","users.id","client_libelles.user_id")
-        // ->groupBy('users.name')
-        // ->where([['client_libelles.user_id',Auth::user()->id],['client_libelles.created_at',$today]])
-        ->get();
-        // dd($byAgent);
+        if ($this->date == "") {
+            $this->all = client::selectRaw("clients.*,users.name un,users.prenom up,client_libelles.*")->orderBy('client_libelles.created_at', "ASC")
+                ->join("client_libelles", "clients.id", "client_libelles.client_id")
+                ->join("users", "users.id", "client_libelles.user_id")
+                ->get();
+        } else{
+            $this->all = client::selectRaw("clients.*,users.name un,users.prenom up,client_libelles.*")->orderBy('client_libelles.created_at', "ASC")
+            ->join("client_libelles", "clients.id", "client_libelles.client_id")
+            ->join("users", "users.id", "client_libelles.user_id")
+            ->whereBetween('client_libelles.created_at', [$this->date . " 00:00:00", $this->date . " 23:59:59"])
+            ->get();
+            // $this->all =$this->date;
+        }
         return view('livewire.dashbord');
     }
 }
